0028ebbb9ac424211984373804f6342cde7ccc58
newlifez1987 <36432389+newlifez1987@users.noreply.github.com>
2018-05-02 09:15:18 +0900

    ys mail

--- src/main/java/com/jhta/project/mail/SimpleMailSender.java
+++ src/main/java/com/jhta/project/mail/SimpleMailSender.java
@@ -1,10 +1,8 @@
 package com.jhta.project.mail;
 
-import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
 
 public class SimpleMailSender {
-	private MailSender ms;
+/*	private MailSender ms;
 
 	public void setMs(MailSender ms) {
 		this.ms = ms;
@@ -23,6 +21,6 @@ public class SimpleMailSender {
 			e.printStackTrace();
 			return false;
 		}
-	}
+	}*/
 
 }
